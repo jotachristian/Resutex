eed7202fd65dbe4e139d7bc6fa26a32bfd638891
João Christian <117664494+jotachristian@users.noreply.github.com>
2024-10-23 14:08:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Resutex/', // Coloque o nome do seu repositório
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react-router-dom']
+    }
+  }
 })
