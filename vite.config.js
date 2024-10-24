f2680605b70a764b3b684e8e68d373b45db50989
João Christian <117664494+jotachristian@users.noreply.github.com>
2024-10-23 21:26:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // Configuração inicial padrão para projetos React com Vite
 export default defineConfig({
   base: '/',
-  plugins: [react()]
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'],
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['react-icons/fa'],
   },
 });
