86b0c6531c6efff467fc93e1c54a7e296c0aa5a5
João Christian <117664494+jotachristian@users.noreply.github.com>
2025-08-18 23:58:15 -0300

    Atualizar o Sobre.jsx

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,4 +1,4 @@
-// src/pages/Sobre.jsx
+²// src/pages/Sobre.jsx
 import React from 'react';
 import SobreIcon from '../assets/Sobre.svg';
 
@@ -14,6 +14,8 @@ function Sobre() {
             <p>
             A crescente digitalização de informações na sociedade contemporânea, evidenciou diversos entraves relacionados à morosidade do tratamento de informações de exames na área da saúde, um desafio multifacetado que afeta tanto indivíduos como organizações. Embora a adoção de tecnologias avançadas tenha promovido eficiência e inovação, o descaso pelo investimento em infraestrutura digital das corporações, a resistência a mudanças por parte dos profissionais, (que muitas vezes estão acostumados a processos manuais) e a carência de treinamentos adequados para o uso dessas tecnologias, são fatores que contribuem para a não implementação de medidas eficazes. Essa situação não apenas compromete a produtividade operacional das instituições de saúde, como afeta diretamente a qualidade do atendimento ao paciente, gerando um ciclo vicioso que prejudica o desenvolvimento sustentável da infraestrutura tecnológica. Esse artigo visa levantar questões acerca da temática, oferecendo uma resolução que possa atender ao corpo social.
             </p>
+
+<h2>Oi, estou testando o github</h2>
             
             </div>
             <div>
